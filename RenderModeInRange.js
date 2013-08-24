a15d4d943767a0ac9061f66036cfc1ccafb24a19
tyson-kubota <tyson.kubota.web@gmail.com>
2013-08-23 20:35:49 -0400

    toggle cave light importance in triggered range

---
+++ RenderModeInRange.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (other : Collider) {
+  if (other.gameObject.CompareTag ("Player")) {
+  	gameObject.light.renderMode = LightRenderMode.ForcePixel;
+  }
+}	
+
+function OnTriggerExit (other : Collider) {
+  if (other.gameObject.CompareTag ("Player")) {
+  	gameObject.light.renderMode = LightRenderMode.Auto;
+  }
+}
