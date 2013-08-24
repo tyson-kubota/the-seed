0ac8fb6389e3b028e56146a3813d58731ea51807
tyson-kubota <tyson.kubota.web@gmail.com>
2013-08-24 12:45:16 -0400

    add underwater lowpass filter

--- Underwater.js
+++ Underwater.js
@@ -2,6 +2,9 @@
 
 var underWaterColor : Color;
 var underWaterDistance : float;
+var underWaterAudioFilter : AudioLowPassFilter;
+var finalCutOffFreq : float = 700;
+var initCutOffFreq : float = 22000;
 
 function Start () {
 }
@@ -16,6 +19,8 @@ function OnTriggerEnter (other : Collider) {
 		Globals.isUnderwater = true;
 		RenderSettings.fogColor = underWaterColor;
 		RenderSettings.fogEndDistance = underWaterDistance;
+		underWaterAudioFilter.enabled = true;
+		yield lerpAudioFilter(.25,FadeDir.In);
 	}
 }
 
@@ -24,5 +29,23 @@ function OnTriggerExit (other : Collider) {
 		Globals.isUnderwater = false;
 		RenderSettings.fogColor = Globals.initFogColor;
 		RenderSettings.fogEndDistance = Globals.initFogEndDist;
+		yield lerpAudioFilter(1,FadeDir.Out);
+		underWaterAudioFilter.enabled = false;
 	}
+}
+
+
+function lerpAudioFilter (timer : float, fadeType : FadeDir) {
+
+    var start = fadeType == FadeDir.In? underWaterAudioFilter.cutoffFrequency : underWaterAudioFilter.cutoffFrequency;
+    var end = fadeType == FadeDir.In? finalCutOffFreq : initCutOffFreq;
+    var i = 0.0;
+    var step = 1.0/timer;
+
+    while (i <= 1.0) {
+    	//if (Globals.doneWaiting == true) {break;}
+        i += step * Time.deltaTime;
+        underWaterAudioFilter.cutoffFrequency = Mathf.Lerp(start, end, i);
+        yield;
+    }
 }
